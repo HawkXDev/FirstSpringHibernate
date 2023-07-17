ef2f76ba42b58a0a51ed7eadde482f2be3bba916
sokolserg <sergsokolkin@gmail.com>
2023-07-17 10:44:06 +0300

    init

---
+++ src/main/java/org/example/springcourse/models/Person.java
@@ -0,0 +1,74 @@
+package org.example.springcourse.models;
+
+import javax.validation.constraints.*;
+
+public class Person {
+    private int id;
+
+    @NotEmpty(message = "Name cannot be empty")
+    @Size(min = 2, max = 30, message = "Name must be between 2 and 30 characters")
+    private String name;
+
+    @Min(value = 0, message = "Age must be greater than 0")
+    private int age;
+
+    @NotEmpty(message = "Email cannot be empty")
+    @Email(message = "Invalid email")
+    private String email;
+
+    // Country, City, ZIP code (6 digits)
+    @Pattern(regexp = "[A-Z]\\w+, [A-Z]\\w+, \\d{6}",
+            message = "Your address must be in the format: Country, City, ZIP code (6 digits)")
+    private String address;
+
+    public Person() {
+    }
+
+    public Person(int id, String name, int age, String email, String address) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.email = email;
+        this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
