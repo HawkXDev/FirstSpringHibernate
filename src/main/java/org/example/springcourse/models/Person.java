f33532787889b21cafbba85273edd047b78ae1bb
sokolserg <sergsokolkin@gmail.com>
2023-07-17 13:40:27 +0300

    Configure Hibernate and Spring, change Person, PersonDAO

--- src/main/java/org/example/springcourse/models/Person.java
+++ src/main/java/org/example/springcourse/models/Person.java
@@ -1,35 +1,31 @@
 package org.example.springcourse.models;
 
+import javax.persistence.*;
 import javax.validation.constraints.*;
 
+@Entity
+@Table(name = "person")
 public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private int id;
 
     @NotEmpty(message = "Name cannot be empty")
     @Size(min = 2, max = 30, message = "Name must be between 2 and 30 characters")
+    @Column(name = "name")
     private String name;
 
     @Min(value = 0, message = "Age must be greater than 0")
+    @Column(name = "age")
     private int age;
-
-    @NotEmpty(message = "Email cannot be empty")
-    @Email(message = "Invalid email")
-    private String email;
-
-    // Country, City, ZIP code (6 digits)
-    @Pattern(regexp = "[A-Z]\\w+, [A-Z]\\w+, \\d{6}",
-            message = "Your address must be in the format: Country, City, ZIP code (6 digits)")
-    private String address;
 
     public Person() {
     }
 
-    public Person(int id, String name, int age, String email, String address) {
-        this.id = id;
+    public Person(String name, int age) {
         this.name = name;
         this.age = age;
-        this.email = email;
-        this.address = address;
     }
 
     public int getId() {
@@ -54,21 +50,5 @@ public class Person {
 
     public void setAge(int age) {
         this.age = age;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
     }
 }
